d37798ffc61207c56c28f20238ad52311b1dc7b2
Christopher Villaran <villaran.christopher@gmail.com>
2021-03-04 16:57:36 +0800

    installed all necessary libraries

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './main.styl';
+
+ReactDom.render(
+  <div>Hello</div>,
+  document.querySelector('#root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode : process.env.NODE_ENV || 'development',
+  devtool: 'source-map',
+  entry: resolve('./src/main.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.(css|styl)$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              sourceMap: true
+            }
+          },
+          {
+            loader: "stylus-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.styl']
+  },
+  output: {
+    path: resolve('./dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+        template : resolve('./src/index.html')
+    }),
+    new MiniCssExtractPlugin()
+  ],
+  devServer: {
+    contentBase: resolve('./dist'),
+  },
+};
+
+function resolve(dir) {
+  return path.resolve(__dirname, dir);
+}
